66028209e91af3acbcb77ec13e69cb7082e4eebf
4miraclevibes <kholid.hasibuan75@gmail.com>
2024-10-03 07:10:34 +0700

    Update main.blade.php

--- resources/views/layouts/backend/main.blade.php
+++ resources/views/layouts/backend/main.blade.php
@@ -16,7 +16,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>Sistem Informasi Program NU</title>
+    <title>GPMI</title>
 
     <meta name="description" content="" />
 
